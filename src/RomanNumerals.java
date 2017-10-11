46853a7547d3865977c64b0a41722fd9670a0b40
tlaivama <tlaivama@ma336-19.student.yo.oulu.fi>
2017-10-11 15:29:55 +0300

    Assignment finished

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,99 @@
 
 public class RomanNumerals {
-	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+	
+	public int convertToInteger(String romanNum) throws IllegalArgumentException {
+		if (!containsOnlyLegalSymbols(romanNum))
+			throw new IllegalArgumentException("Numeral contained an illegal symbol");
 		
+		if (!containsOnlyLegalRepetitionOfSymbols(romanNum))
+			throw new IllegalArgumentException("Numeral contained illegal repetition");
+		
+		if (!containsSymbolsInLegalOrder(romanNum))
+			throw new IllegalArgumentException("Numeral contained symbols in illegal order");
+		
+		return calculateArabicFromRomanNumeral(romanNum);
 	}
+	
+	private boolean containsOnlyLegalSymbols(String romanNum) {
+		return romanNum.matches("[IVXLCDM]*");
+	}
+	
+	private boolean containsOnlyLegalRepetitionOfSymbols(String romanNum) {
+		return checkLegalityOfRepeatableSymbols(romanNum) && checkLegalityOfNonRepeatableSymbols(romanNum);
+	}
+	
+	private boolean checkLegalityOfRepeatableSymbols(String romanNum) {
+		String repeatableSymbols = "IXCM";
+		
+		for (char repeatableSymbol : repeatableSymbols.toCharArray()) {
+			int repetitions = 0;
+			
+			for (char symbol : romanNum.toCharArray())
+				if (symbol == repeatableSymbol)
+					++repetitions;
+			
+			if (repetitions > 3)
+				return false;
+		}
+		
+		return true;
+	}
+	
+	private boolean checkLegalityOfNonRepeatableSymbols(String romanNum) {
+		String nonRepeatableSymbols = "VLD";
+		
+		for (char nonRepeatableSymbol : nonRepeatableSymbols.toCharArray()) {
+			int repetitions = 0;
+			
+			for (char symbol : romanNum.toCharArray())
+				if (symbol == nonRepeatableSymbol)
+					++repetitions;
+			
+			if (repetitions > 1)
+				return false;
+		}
+		
+		return true;
+	}
+	
+	/*
+	 * Checks that only one subtraction is made (things like "XXC" will return false),
+	 * the "5" symbols are not used in subtraction and
+	 * that the "1" symbols can only be subtracted from the next 2 highest values
+	 */
+	private boolean containsSymbolsInLegalOrder(String romanNum) {
+		String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+		
+		String num = romanNum;
+		for (int i = 0; i < roman.length; ++i) {
+			while (num.startsWith(roman[i])) {
+				num = num.substring(roman[i].length());
+			}
+			
+			for (int j = 0; j < i; ++j) {
+				if (num.startsWith(roman[j]))
+					return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	private int calculateArabicFromRomanNumeral(String romanNum) {
+		int[] arabic = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+		String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+		
+		int result = 0;
+		String num = romanNum;
+		
+		for (int i = 0; i < arabic.length; ++i) {
+			while (num.startsWith(roman[i])) {
+				result += arabic[i];
+				num = num.substring(roman[i].length());
+			}
+		}
+		
+		return result;
+	}
+	
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -5,8 +5,97 @@ import org.junit.Test;
 public class TestRomanNumerals {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRomanNumerals_AcceptsLegalSymbols() {
+		RomanNumerals num = new RomanNumerals();
+		
+		try {
+			num.convertToInteger("MDCLXVI");
+		}
+		catch (IllegalArgumentException e) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void testRomanNumerals_ThrowsExceptionForIllegalSymbols() {
+		RomanNumerals num = new RomanNumerals();
+		
+		try {
+			num.convertToInteger("A");
+			fail();
+		}
+		catch (IllegalArgumentException e) {
+			// Pass
+		}
+	}
+	
+	@Test
+	public void testRomanNumerals_CanContainLegalRepetition() {
+		RomanNumerals num = new RomanNumerals();
+		
+		try {
+			num.convertToInteger("MCMLXXXIV");
+		}
+		catch (IllegalArgumentException e) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void testRomanNumerals_ThrowsExceptionForTooMuchRepetition() {
+		RomanNumerals num = new RomanNumerals();
+		
+		try {
+			num.convertToInteger("MMMCMLXXXIV");
+			fail();
+		}
+		catch (IllegalArgumentException e) {
+			// Pass
+		}
+	}
+	
+	@Test
+	public void testRomanNumerals_ThrowsExceptionForIllegalRepetition() {
+		RomanNumerals num = new RomanNumerals();
+		
+		try {
+			num.convertToInteger("DDC");
+			fail();
+		}
+		catch (IllegalArgumentException e) {
+			// Pass
+		}
+	}
+	
+	@Test
+	public void testRomanNumerals_ThrowsExceptionForIllegalSymbolOrder() {
+		RomanNumerals num = new RomanNumerals();
+		
+		try {
+			num.convertToInteger("XXC");
+			fail();
+		}
+		catch (IllegalArgumentException e) {
+			// Pass
+		}
+	}
+	
+	@Test
+	public void testRomanNumerals_ReturnsCorrectResultForDCCC() {
+		RomanNumerals num = new RomanNumerals();
+		assertEquals(800, num.convertToInteger("DCCC"));
+	}
+	
+	@Test
+	public void testRomanNumerals_ReturnsCorrectResultForMCMLXXXIV() {
+		RomanNumerals num = new RomanNumerals();
+		assertEquals(1984, num.convertToInteger("MCMLXXXIV"));
+	}
+	
+	@Test
+	public void testRomanNumerals_ReturnsCorrectResultForMMXIV() {
+		RomanNumerals num = new RomanNumerals();
+		assertEquals(2014, num.convertToInteger("MMXIV"));
 	}
 
 }
